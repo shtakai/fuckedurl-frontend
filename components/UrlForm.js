7104d627a66296b2674c779786b7076b000aefe6
Alyson Takai <shtakai@gmail.com>
2019-01-02 20:31:47 +0700

    WIP

--- components/UrlForm.js
+++ components/UrlForm.js
@@ -1,7 +1,7 @@
 import { Component } from 'react'
 import isUri from 'validate.io-uri'
-
 import fetch from 'isomorphic-unfetch'
+import Url from 'url-parse'
 
 class UrlForm extends Component {
   constructor(props) {
@@ -13,16 +13,42 @@ class UrlForm extends Component {
 
     this.handleCreateUrl = this.handleCreateUrl.bind(this)
     this.updateUrl = this.updateUrl.bind(this)
+    this.urlGenerated = this.urlGenerated.bind(this)
 
   }
+
+  urlGenerated(url) {
+    this.props.urlGenerated(url)
+  }
+
   handleCreateUrl(e) {
+    console.log(`-----------`)
+    console.log(`urlGenerated:${this.state.urlGenerated}`)
+    console.log(`-----------`)
     console.log('handleCreateUrl')
     console.log(this.state.url)
     let isValid = isUri(this.state.url)
-    console.log(`FUCKED URL IS VALID?: ${isValid}`)
-    fetch(`http://localhost:3000/api/v1/links`)
+    let url = this.state.url
+    let fullUrl = new Url(window.location.href)
+    let baseUrl = `${fullUrl.protocol}//${fullUrl.host}/r/`
+    console.log(`${url} FUCKED URL IS VALID?: ${isValid}`)
+    fetch(`http://localhost:3000/api/v1/links/create`, {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        url: url
+      })
+      })
       .then( r => r.json() )
-      .then( data => console.log(data) )
+      .then( data => {
+        console.log(data)
+        let redirectUrl = `${baseUrl}${data.short_id}`
+        console.log(`URL has been shortened. => ${redirectUrl}`)
+        this.urlGenerated(redirectUrl)
+      })
   }
 
   updateUrl(e) {
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,41 @@
+import { Component } from 'react'
+
 import Layout from '../components/Layout'
 import UrlForm from '../components/UrlForm'
+import Url from '../components/Url'
 
-const Index = (props) => (
-  <Layout>
-    <div>
-      <h1>Here we are FuckURL</h1>
-      <UrlForm/>
-    </div>
-  </Layout>
-)
+class Index extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      url: '',
+    }
+
+    this.urlGenerated = this.urlGenerated.bind(this)
+  }
+
+  urlGenerated(url) {
+    console.log('emit fuck')
+    console.log(url)
+    this.setState({ url: url })
+  }
+
+  render() {
+    return(
+      <Layout>
+        <div>
+          <h1>Here we are FuckURL</h1>
+          <UrlForm urlGenerated={this.urlGenerated}/>
+          {this.state.url && <Url url={this.state.url}/>}
+        </div>
+      </Layout>
+    )
+  }
+}
+
+// const Index = (props) => (
+// )
 
 export default Index
 
